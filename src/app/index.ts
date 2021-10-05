e31a45f7bff60802e99d0695558000af2956e2cc
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2021-10-05 13:09:36 +0200

    Streamline genrator experience

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,32 +1,40 @@
 import * as chalk from 'chalk';
+import GeneratorSelection from '../GeneratorSelection';
 import BaseSelectionGenerator from '../BaseSelectionGenerator';
 const yosay = require('yosay');
 import { resolveGeneratorInheritance } from '../GeneratorResolver';
+import SeparatorSelection from '../SeparatorSelection';
 
 class AppGenerator extends BaseSelectionGenerator {
   constructor(args: any, options: any) {
     super(args, options);
     this.generators = [
-      {
-        name: 'New wemogy project component',
-        generator: 'wemogy:project'
-      },
-      {
-        name: 'Single template',
-        generator: 'wemogy:template'
-      }
+      new GeneratorSelection('wemogy Empty project structure', 'wemogy:project-empty'),
+      new GeneratorSelection('wemogy Service / Microservice (.NET)', 'wemogy:project-service-dotnet'),
+      new GeneratorSelection('wemogy Class Library (.NET)', 'wemogy:project-lib-dotnet'),
+      new GeneratorSelection('wemogy Web Frontend (React)', 'wemogy:project-frontend-react'),
+      new GeneratorSelection('wemogy SDK (.NET)', 'wemogy:project-sdk-dotnet'),
+      new GeneratorSelection('wemogy SDK (JavaScript)', 'wemogy:project-sdk-javascript'),
+      new SeparatorSelection(),
+      new GeneratorSelection('.NET', 'wemogy:dotnet'),
+      new GeneratorSelection('TypeScript', 'wemogy:typescript'),
+      new GeneratorSelection('Terraform', 'wemogy:terraform'),
+      new GeneratorSelection('Yeoman', 'wemogy:yeoman'),
+      new GeneratorSelection('GitHub Actions', 'wemogy:github-actions'),
+      new SeparatorSelection()
     ];
+
     this.log(yosay(`Welcome to the ${chalk.blue(`wemogy`)} code generator!`));
     this.log(
-      `Choose "${chalk.cyan(
-        this.generators[0].name
-      )}" to generate a new project component (part of a larger wemogy project) for a wemogy repository.`
+      `Choose generators with the ${chalk.cyan(
+        'wemogy'
+      )} prefix, to generate new projects as part of a larger wemogy repository. This will automatically create the correct folder structure.`
     );
-    this.log('This will automatically create the correct folder structure.');
-    this.log(`${chalk.yellow('Hint:')} Please make sure to call this generator from the repository root.`);
+    this.log(chalk.yellow('Hint: Please make sure to call this generator from the repository root.'));
     this.log('');
-    this.log(`Choose "${chalk.cyan(this.generators[1].name)}" to generate basic templates for specific technologies.`);
-    this.log('This will not generate any specific folder structure.');
+    this.log(
+      'All other options generate basic templates for specific technologies without any specific folder structure.'
+    );
     this.log('');
   }
 }
--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,15 +1,13 @@
 import { resolveGeneratorInheritance } from '../GeneratorResolver';
 import BaseSelectionGenerator from '../BaseSelectionGenerator';
+import GeneratorSelection from '../GeneratorSelection';
 
 class TypeScriptGenerator extends BaseSelectionGenerator {
   constructor(args: any, options: any) {
     super(args, options);
-
     this.generators = [
-      {
-        name: 'React.js',
-        generator: 'wemogy:typescript-react'
-      }
+      new GeneratorSelection('Empty', 'wemogy:typescript-empty'),
+      new GeneratorSelection('React', 'wemogy:typescript-react')
     ];
   }
 }
