55548f05c5337fcc58e701e3667658875525eef8
Sebastian Küsters <sebastian@wemogy.com>
2021-11-05 16:48:25 +0100

    BaseTemplateGenerator updated

--- src/BaseTemplateGenerator.ts
+++ src/BaseTemplateGenerator.ts
@@ -1,3 +1,4 @@
+import _ = require('lodash');
 import * as Generator from 'yeoman-generator';
 import optionOrPrompt, { AdvancedQuestions } from './OptionOrPrompt';
 
@@ -31,6 +32,42 @@ class BaseTemplateGenerator extends Generator {
     this.log('Applying EditorConfig rules by running eclint...');
     this.spawnCommandSync('eclint', ['fix', '$(git ls-files)'], { shell: true });
   }
+
+  protected copyTemplateToDestination(destinationSubPath: string, params?: object): void {
+    if (!params) {
+      params = {};
+    }
+
+    const args = { ...this.answers, ...params };
+    const argNames = Object.keys(args);
+
+    this.fs.copyTpl(
+      this.templatePath(),
+      `${this.destinationPath()}/${destinationSubPath}`,
+      args,
+      {},
+      {
+        globOptions: {
+          dot: true // Include dotfiles (like .stylecop)
+        },
+        processDestinationPath: (filePath: string): string => {
+          for (let argName of argNames) {
+            filePath = filePath.replace(`$${argName}`, this.upperCamelCase(args[argName]));
+          }
+
+          return filePath;
+        }
+      }
+    );
+  }
+
+  //#region conventions
+
+  protected upperCamelCase(str: string): string {
+    return _.upperFirst(_.camelCase(str));
+  }
+
+  //#endregion
 }
 
 export default BaseTemplateGenerator;
