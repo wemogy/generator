c758a9f9bc820ca2ad748c6bbd4fc61c5d6092e9
Sebastian Küsters <sebastian@wemogy.com>
2021-11-05 18:12:23 +0100

    wemogy-cli-command-draft

---
+++ src/wemogy-cli-command/index.ts
@@ -0,0 +1,44 @@
+import _ = require('lodash');
+import BaseTemplateGenerator from '../BaseTemplateGenerator';
+
+class WemogyCliCommand extends BaseTemplateGenerator {
+  constructor(args: any, options: any) {
+    super(args, options);
+  }
+
+  // Your initialization methods (checking current project state, getting configs, etc
+  public initialize(): void {}
+
+  // Where you prompt users for options (where you’d call this.prompt())
+  public async prompting() {
+    this.answers = await this.optionOrPrompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Command name'
+      },
+      {
+        type: 'input',
+        name: 'path',
+        message: 'Command path'
+      }
+    ]);
+  }
+
+  // Saving configurations and configure the project (creating .editorconfig files and other metadata files
+  public configuring(): void {}
+
+  //  Where you write the generator specific files (routes, controllers, etc)
+  public writing(): void {
+    const namespace = this.pascalCase(this.answers.path.replace('/', ' '));
+    this.copyTemplateToDestination(this.answers.path, { namespace });
+  }
+
+  // Where installation are run (npm, bower)
+  public install(): void {}
+
+  // Called last, cleanup, say good bye, etc
+  public end(): void {}
+}
+
+export default WemogyCliCommand;
