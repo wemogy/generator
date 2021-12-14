f086445e520f1071897b60ec0ab857e29dfe0106
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2021-12-14 12:24:32 +0100

    Fix Kubernetes Config Generator

--- src/terraform-kubernetes-config/index.ts
+++ src/terraform-kubernetes-config/index.ts
@@ -1,7 +1,7 @@
 const yosay = require('yosay');
 import BaseTemplateGenerator from '../BaseTemplateGenerator';
 
-class TerraformAksGenerator extends BaseTemplateGenerator {
+class TerraformKubernetesConfigGenerator extends BaseTemplateGenerator {
   constructor(args: any, options: any) {
     super(args, options);
   }
@@ -15,62 +15,15 @@ class TerraformAksGenerator extends BaseTemplateGenerator {
       {
         type: 'input',
         name: 'id',
-        message: 'Terraform ID',
-        default: 'default'
+        message: 'Ingress: controller.service.loadBalancerIP',
+        default: 'azurerm_public_ip.ingress_ip.ip_address'
       },
       {
         type: 'input',
         name: 'name',
-        message: 'Resource Name',
-        default: '"${var.prefix}aks"'
-      },
-      {
-        type: 'confirm',
-        name: 'publicIp',
-        message: 'Create public IP address for Ingress?'
-      },
-      {
-        when: (answers: any) => answers.publicIp,
-        type: 'input',
-        name: 'publicIpName',
-        message: 'Public IP address Resource Name',
-        default: '"${var.prefix}aksingress"'
-      },
-      {
-        type: 'input',
-        name: 'vnetName',
-        message: 'Virtual Network Name',
-        default: '"${var.prefix}vnet"'
-      },
-      {
-        type: 'input',
-        name: 'resourceGroupName',
-        message: 'Azure Resource Group Name',
+        message:
+          'Ingress: controller.service.annotations.service\\.beta\\.kubernetes\\.io\\/azure-load-balancer-resource-group',
         default: 'azurerm_resource_group.default.name'
-      },
-      {
-        when: (answers: any) => answers.publicIp,
-        type: 'input',
-        name: 'resourceGroupId',
-        message: 'Azure Resource Group ID',
-        default: 'azurerm_resource_group.default.id'
-      },
-      {
-        type: 'input',
-        name: 'location',
-        message: 'Azure Location',
-        default: 'azurerm_resource_group.default.location'
-      },
-      {
-        type: 'input',
-        name: 'kubernetesVersion',
-        message: 'Kubernetes Version',
-        default: '1.20.7'
-      },
-      {
-        type: 'confirm',
-        name: 'createProviders',
-        message: 'Create Terraform providers for this cluster?'
       }
     ]);
   }
@@ -80,38 +33,14 @@ class TerraformAksGenerator extends BaseTemplateGenerator {
 
   //  Where you write the generator specific files (routes, controllers, etc)
   public writing(): void {
-    // AKS
-    this.fs.copyTpl(this.templatePath('azure_aks.tf'), this.destinationPath('azure_aks.tf'), this.answers);
-    this.fs.copyTpl(
-      this.templatePath('azure_networking.tf'),
-      this.destinationPath('azure_networking.tf'),
-      this.answers
-    );
-
-    // RBAC
-    if (this.answers.publicIp) {
-      this.fs.copyTpl(this.templatePath('azure_rbac.tf'), this.destinationPath('azure_rbac.tf'), this.answers);
-    }
-
-    // Providers
-    if (this.answers.createProviders) {
-      this.fs.copyTpl(
-        this.templatePath('provider_kubernetes.tf'),
-        this.destinationPath('provider_kubernetes.tf'),
-        this.answers
-      );
-    }
+    this.copyTemplateToDestination();
   }
 
   // Where installation are run (npm, bower)
   public install(): void {}
 
   // Called last, cleanup, say good bye, etc
-  public end(): void {
-    this.log(
-      'Project structure has been created. Please checkout our docs for details: https://docs.wemogy.com/docs-internal/devops/terraform'
-    );
-  }
+  public end(): void {}
 }
 
-export default TerraformAksGenerator;
+export default TerraformKubernetesConfigGenerator;
