6d524995e8f5bd24cb291d25b2f2c0f0b367a4c2
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2023-07-26 15:53:55 +0200

    Fix updater

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,8 @@
-import * as chalk from 'chalk';
-import * as Generator from 'yeoman-generator';
-import {UpdateInfo, UpdateNotifier} from 'update-notifier';
+import * as chalk from "chalk";
+import * as Generator from "yeoman-generator";
+import { UpdateInfo, UpdateNotifier } from "update-notifier";
 
-const yosay = require('yosay');
+const yosay = require("yosay");
 
 class AppGenerator extends Generator {
   private answers: any;
@@ -14,40 +14,44 @@ class AppGenerator extends Generator {
 
   public async initialize() {
     // Check for updates
-    const packageJson = require('../../package.json');
+    const packageJson = require("../../package.json");
     var notifier = new UpdateNotifier({
-      pkg: packageJson
+      pkg: packageJson,
     });
 
     this.updateInfo = await notifier.fetchInfo();
 
-    if (this.updateInfo.type == 'latest') {
+    if (this.updateInfo.type == "latest") {
       // Generator is on the latest version
       this.log(yosay(`Welcome to the ${chalk.blue(`wemogy`)} code generator!`));
     } else {
       // Generator is NOT on the latest version
       this.log(
-        yosay(`Update available!\n ${chalk.blue(this.updateInfo.current)} -> ${chalk.green(this.updateInfo.latest)}`)
+        yosay(
+          `Update available!\n ${chalk.blue(
+            this.updateInfo.current
+          )} -> ${chalk.green(this.updateInfo.latest)}`
+        )
       );
       this.answers = await this.prompt([
         {
-          type: 'confirm',
-          name: 'update',
-          message: 'What do you want to update now?',
-          default: true
-        }
+          type: "confirm",
+          name: "update",
+          message: "Do you want to update now?",
+          default: true,
+        },
       ]);
     }
   }
 
   public writing(): void {
     if (this.answers?.update) {
-      this.log('Installing update...');
-      this.spawnCommandSync('npm', ['upgrade', '-g', 'generator-wemogy']);
-      this.log('Update installed.');
-      this.composeWith('wemogy:app');
+      this.log("Installing update...");
+      this.spawnCommandSync("npm", ["upgrade", "-g", "generator-wemogy"]);
+      this.log("Update installed.");
+      return;
     } else {
-      this.composeWith('wemogy:app-selection');
+      this.composeWith("wemogy:app-selection");
     }
   }
 }
